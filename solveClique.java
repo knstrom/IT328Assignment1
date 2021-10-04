57d8d44739a29fe3441e13533b054286436bf6ea
khartman428 <k.hartman428@gmail.com>
2021-10-04 14:18:13 -0500

    Inital branch commit

---
+++ solveClique.java
@@ -0,0 +1,28 @@
+/*
+IT 328: Programming Assignment 1
+Katelyn Hartman & Kaci Stromberger
+Find K-Clique for given undirected graph
+9/27/2021
+*/
+
+public class solveClique
+{
+    public static void main(String args[])
+    {
+        /* Read Adjacency Matrix from graphs2021.txt*/
+
+        /* Find Number vertices and edges */
+
+        /* Find Maximum k-clique */
+
+        /* Print in following format:
+                Max Cliques in graphs in graphs2021.txt
+                (|V|,|E|) Cliques (size, ms used)
+                G1 (5, 2) {0,4} (size=2, 0 ms)
+                G2 (5, 4) {0,1,4} (size=3, 0 ms)
+                G3 (5, 6) {0,2,4} (size=3, 0 ms)
+                ............
+                ............
+                G100 (100,3428) {4,6,7,11,24,32,33,39,41,67,68,84,88,94 } (size=14, 8011 ms) */
+    }
+}
